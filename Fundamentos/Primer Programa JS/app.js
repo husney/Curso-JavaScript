d6da613fbba0b103dac4649f9271d3e1a7650d38
Husney <usneibayon@gmail.com>
2020-08-19 20:38:24 -0500

    Primer programa en JS

---
+++ Fundamentos/Primer Programa JS/app.js
@@ -0,0 +1,4 @@
+let nombre = prompt("¿Cual es tú nombre?");
+let edad = prompt("¿Cual es tú edad?");
+
+document.getElementById("persona").innerHTML = "Bienvenido " + nombre + " de " + edad + " años";
