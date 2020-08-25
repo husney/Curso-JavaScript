3591213285c8b827148542aaa410af6073d689f8
Husney <usneibayon@gmail.com>
2020-08-25 18:56:44 -0500

    Operadores

---
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -0,0 +1,47 @@
+/* let n1 = 20;
+let n2 = 30;
+let n3 = "20";
+
+console.log(n2 >= n1); //Es mayor
+console.log(n1 >= n3); //El valor es igual
+console.log("_________");
+
+console.log(n1 <= n2); //Es menor
+console.log(n1 <= n3); //El valor es igual
+console.log("_________");
+
+console.log(n1 != n3); //Es el mismo valor
+console.log(n1 != n2); //Son valores diferentes
+
+console.log(n1 !== n3); //Es el mismo valor Pero no el mismo tipo de dato
+console.log("_________");
+
+console.log(n1 == n3); //Es el mismo valor
+
+console.log(n1 === n3); //Es el mismo valor pero no el mismo tipo de dato
+ */
+
+
+
+
+/* console.log(n1 == n3);
+console.log("_________");
+
+console.log(n1 === n3);
+console.log("_________");
+
+
+console.log(n1 >= n2);
+console.log(n1 >= n3);
+console.log("_________");
+
+console.log(n1 <= n2);
+console.log(n2 <= n1);
+console.log(n1 <= n3);
+console.log("_________");
+
+console.log(n1 != n2);
+console.log(n1 != n3);
+console.log(n1 !== n3);
+
+ */
