b8e3a6083de5824d0090bbd2e681e72f8c406cfe
husney_bayona <husney_bayona@cnt.com.co>
2021-01-11 18:51:07 -0500

    Operadores

---
+++ Curso JS Moderno/05-Operadores/js/02-app.js
@@ -0,0 +1,13 @@
+let n1 = 5;
+let n2 = 1;
+let n3 = "5";
+
+console.log(n1 == n3)
+console.log(n1 == n2);
+
+console.log(n1 === n3);
+console.log("-------");
+
+console.log(n1 != n3);
+console.log(n1 !== n3);
+
---
+++ Curso JS Moderno/05-Operadores/js/03-app.js
@@ -0,0 +1,9 @@
+let n1 = null;
+let n2;
+
+console.log(n1);
+console.log(n2)
+
+console.log(n1 == n2);
+console.log(typeof(n1), typeof(n2));
+console.log(n1 === n2);
